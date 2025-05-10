6037e2370317a85dedfa688b319ab3bf80793f88
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-10 17:58:29 +0300

    Add Laravel Debugbar for enhanced debugging and disable it in AppServiceProvider
    - Added "barryvdh/laravel-debugbar" as a development dependency in composer.json
    - Updated composer.lock to include the new Debugbar package
    - Modified AppServiceProvider to disable Debugbar during service registration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Barryvdh\Debugbar\Facades\Debugbar;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
+    public function register(): void {
+        Debugbar::disable();
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
+    public function boot(): void {
         //
     }
 }
