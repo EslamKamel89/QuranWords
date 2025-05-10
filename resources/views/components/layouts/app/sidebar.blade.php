671237f59c8c8c99839290171fa21ff078fd8dd6
EslamKamel89 <elsamkamelforex@gmail.com>
2025-05-10 21:06:47 +0300

    feat: enhance sidebar layout with toggle icons and clean up unused code

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -9,8 +9,9 @@ $isRtl = app()->getLocale() === 'ar';
 </head>
 
 <body class="flex flex-row min-h-screen bg-white dark:bg-zinc-800" x-data="{show:true}">
-    <div class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
-        <flux:icon.list-bullet class="block m-4 lg:hidden" @click="show=!show" />
+    <div class="pt-0 border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900 lg:pt-14">
+        <flux:icon.bars-2 class="block m-4 lg:hidden" @click="show=!show" x-show="!show" />
+        <flux:icon.x-mark class="block m-4 lg:hidden" @click="show=!show" x-show="show" />
         <flux:sidebar sticky class="hidden lg:block" x-bind:class="{'!block':show}">
 
             <a href="{{ route('dashboard') }}" class="flex items-center space-x-2 me-5 rtl:space-x-reverse" wire:navigate>
@@ -24,17 +25,7 @@ $isRtl = app()->getLocale() === 'ar';
             </flux:navlist>
 
             <flux:spacer />
-            <!--
-        <flux:navlist variant="outline">
-            <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
-                {{ __('Repository') }}
-            </flux:navlist.item>
 
-            <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
-                {{ __('Documentation') }}
-            </flux:navlist.item>
-        </flux:navlist>
--->
 
             <!-- Desktop User Menu -->
             <flux:dropdown position="bottom" align="start">
@@ -80,59 +71,7 @@ $isRtl = app()->getLocale() === 'ar';
             </flux:dropdown>
         </flux:sidebar>
     </div>
-
-    <!-- Mobile User Menu -->
-    <!--
-    <flux:header class="lg:hidden">
-        <flux:sidebar.toggle
-            @click="$dispatch('toggle-sidebar', { id: 'main-sidebar' })"
-            class="lg:hidden" icon="bars-2" inset="left" />
-
-        <flux:spacer />
-
-        <flux:dropdown position="top" align="start">
-            <flux:profile
-                :initials="auth()->user()->initials()"
-                icon-trailing="chevron-down" />
-
-            <flux:menu>
-                <flux:menu.radio.group>
-                    <div class="p-0 text-sm font-normal">
-                        <div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
-                            <span class="relative flex w-8 h-8 overflow-hidden rounded-lg shrink-0">
-                                <span
-                                    class="flex items-center justify-center w-full h-full text-black rounded-lg bg-neutral-200 dark:bg-neutral-700 dark:text-white">
-                                    {{ auth()->user()->initials() }}
-                                </span>
-                            </span>
-
-                            <div class="grid flex-1 text-sm leading-tight text-start">
-                                <span class="font-semibold truncate">{{ auth()->user()->name }}</span>
-                                <span class="text-xs truncate">{{ auth()->user()->email }}</span>
-                            </div>
-                        </div>
-                    </div>
-                </flux:menu.radio.group>
-
-                <flux:menu.separator />
-
-                <flux:menu.radio.group>
-                    <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
-                </flux:menu.radio.group>
-
-                <flux:menu.separator />
-
-                <form method="POST" action="{{ route('logout') }}" class="w-full">
-                    @csrf
-                    <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
-                        {{ __('Log Out') }}
-                    </flux:menu.item>
-                </form>
-            </flux:menu>
-        </flux:dropdown>
-    </flux:header>
--->
-    <div>
+    <div class="w-full">
 
         {{ $slot }}
     </div>
